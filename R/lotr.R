9e0f536134bc3aa0d83792ea6201095b617bdd4a
Simon Jorstedt <jorstedtsimon@gmail.com>
2023-10-05 10:10:04 +0200

    Package setup
    Various setup things following the readme instructions (authored by me)

---
+++ R/lotr.R
@@ -0,0 +1,31 @@
+#' LOTR
+#'
+#' @description A function for connection with the LOTR API.
+#'
+#' @param arg1 A short description of the arg1 argument. Must specify: accepted format.
+#'
+#' @param arg2 A short description of the arg2 argument. Must specify: accepted format.
+#'
+#' @return A short description of the output format.
+#'
+#' @importFrom magrittr %>%
+#' @importFrom dplyr arrange filter
+#'
+#' @source [The one API](https://the-one-api.dev/)
+#'
+#' @export lotr
+
+
+lotr <- function(arg1, arg2){
+  print("What is this new devilry?")
+
+  # ---V--- CHECK INPUT ---V---
+  # Check arg1 argument
+  #stopifnot("argument \"arg1\" is not ---" = is.---(arg1))
+
+  # Check arg2 argument
+  #stopifnot("argument \"arg2\" is not ---" = is.---(arg2))
+  # ---^--- CHECK INPUT ---^---
+
+  return()
+}
---
+++ tests/testthat/test-lotr.R
@@ -0,0 +1,18 @@
+# test_that("Text explaining what this test will test", {
+#   expect_equal(new_function(arg1, arg2), expected_output)
+# })
+#
+# test_that("The function works as intended", {
+#   expect_equal(new_function(arg1, arg2), expected_output)
+# })
+#
+# test_that("Errors are discovered", {
+#   expect_error(new_function(arg1, arg2))
+# })
+#
+# test_that("Output is as expected", {
+#   expect_output(new_function(arg1, arg2), "regex thing")
+#   expect_output(new_function(arg1, arg2), "regex thing")
+#   expect_output(new_function(arg1, arg2), "\\d\\d\\d***\\(\\)")
+#   expect_output(new_function(arg1, arg2), "A number: \d Anything:*")
+# })
