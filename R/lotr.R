f7c96f114fa9abfd0f68687175b609ced8dee22b
Simon Jorstedt <jorstedtsimon@gmail.com>
2023-10-05 16:52:23 +0200

    Created the basic client structure
    Currently it successfully sends requests for characters in lotr.

--- R/lotr.R
+++ R/lotr.R
@@ -9,7 +9,9 @@
 #' @return A short description of the output format.
 #'
 #' @importFrom magrittr %>%
-#' @importFrom dplyr arrange filter
+#' @importFrom dplyr arrange filter as_tibble
+#' @importFrom httr GET add_headers content
+#' @importFrom jsonlite fromJSON
 #'
 #' @source [The one API](https://the-one-api.dev/)
 #'
@@ -17,7 +19,6 @@
 
 
 lotr <- function(arg1, arg2){
-  print("What is this new devilry?")
 
   # ---V--- CHECK INPUT ---V---
   # Check arg1 argument
@@ -27,5 +28,31 @@ lotr <- function(arg1, arg2){
   #stopifnot("argument \"arg2\" is not ---" = is.---(arg2))
   # ---^--- CHECK INPUT ---^---
 
-  return()
+  base_url <- "https://the-one-api.dev/v2"
+  end_point <- "/character"
+  query_url <- paste0(base_url, end_point)
+  response <- GET(query_url, add_headers(Authorization=paste("Bearer", Sys.getenv("lotr_id"))))
+
+  if(response$status_code == 401){print("Status code 401: Unauthorized. The request lacks valid authentication.")}
+  else if(response$status_code == 404){print("Status code 404: could not find a client-requested webpage")}
+  else if(response$status_code == 429){print("Status code 429: Too many requests. You have been rate limited.")}
+  else if(response$status_code == 200){print("Status code 200: The request was OK/successsful.")}
+  else {cat("Unknown status code: ", response$status_code)}
+
+  # Format the response
+  response <- fromJSON(content(response, as="text"))
+
+  response_data <- as_tibble(response$docs)
+
+  #View(response)
+
+  # Return the response to the user
+  return(response_data)
 }
+
+# The one API rate limit: 100 requests per 10 min, or one every 6 seconds.
+
+#response <- lotr()
+
+
+
